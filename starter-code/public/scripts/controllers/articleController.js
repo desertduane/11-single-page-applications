fe26577c87589e35eabca30919b49ff39f378f3c
Erik Plyushko <eplyushko@gmail.com>
2017-08-28 15:32:09 -0700

    erik-duane

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -3,14 +3,14 @@ var app = app || {};
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
   app.Article.fetchAll(app.articleView.initIndexPage);
   articleController.init = function() {
-  $('.tab-content').hide();
-  $('#articles').show();
-}
+    $('.tab-content').hide();
+    $('#articles').show();
+  }
 
   module.articleController = articleController;
 })(app);
