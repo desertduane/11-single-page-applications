7ad4cca9d0f6fd8c869183b0cdde5ed587f66372
Duane Rush <desertduane@yahoo.com>
2017-08-28 15:23:25 -0700

    all todos complete

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,17 +5,10 @@ var app = app || {};
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-  aboutController.init = function() {
-    $('.tab').on('click', 'li', function() {
-      if($(this).attr('data-content') === 'about' || $(this).attr('data-content') === 'articles') {
-        $('main').hide();
-        $('#' + $(this).attr('data-content')).fadeIn();
-      } else {
-        $('main').fadeIn();
-      }
-    })
-  }
-
+aboutController.init = function() {
+  $('.tab-content').hide();
+  $('#about').show();
+}
 
   module.aboutController = aboutController;
 })(app);
--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -6,8 +6,11 @@ var app = app || {};
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
-
+  app.Article.fetchAll(app.articleView.initIndexPage);
+  articleController.init = function() {
+  $('.tab-content').hide();
+  $('#articles').show();
+}
 
   module.articleController = articleController;
 })(app);
